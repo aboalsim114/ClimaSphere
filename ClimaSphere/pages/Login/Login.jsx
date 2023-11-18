67e1b24c080d127f71cd1cf72b77963011874a82
“aboalsim114” <aboalsim114@gmail.com>
2023-11-19 00:10:39 +0100

    Implémenter un système d'authentification Firebase

--- ClimaSphere/pages/Login/Login.jsx
+++ ClimaSphere/pages/Login/Login.jsx
@@ -1,28 +1,53 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator, Alert } from "react-native";
 import { styles } from "./LoginStyle";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
     const [emailError, setEmailError] = useState("");
     const [passwordError, setPasswordError] = useState("");
+    const auth = getAuth();
 
+    const LoginUser = async () => {
+        if (!validateEmail() || !validatePassword()) return;
+        setLoading(true);
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            Alert.alert("connexion reussi")
+        } catch (error) {
+            if (error.code === 'auth/user-not-found') {
+                setEmailError('No user found with this email.');
+            } else if (error.code === 'auth/wrong-password') {
+                setPasswordError('Incorrect password.');
+            } else {
+                setEmailError('Failed to login. Please try again.');
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const validateEmail = () => {
         if (email.length === 0 || !email.includes('@')) {
             setEmailError("Email invalide");
+            return false;
         } else {
             setEmailError("");
+            return true;
         }
     };
-
 
     const validatePassword = () => {
         if (password.length === 0) {
             setPasswordError("Password invalide");
+            return false;
         } else {
             setPasswordError("");
+            return true;
         }
     };
 
@@ -38,7 +63,7 @@ export default function Login() {
                     placeholder="Email"
                     keyboardType="email-address"
                     placeholderTextColor="#ffffff"
-                    onChangeText={(text) => setEmail(text)}
+                    onChangeText={setEmail}
                     onEndEditing={validateEmail}
                     value={email}
                 />
@@ -51,16 +76,20 @@ export default function Login() {
                     placeholder="Password"
                     secureTextEntry
                     placeholderTextColor="#ffffff"
-                    onChangeText={(text) => setPassword(text)}
+                    onChangeText={setPassword}
                     onEndEditing={validatePassword}
                     value={password}
                 />
                 {!!passwordError && <Text style={styles.error}>{passwordError}</Text>}
             </View>
 
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Connexion</Text>
-            </TouchableOpacity>
+            {loading ? (
+                <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+                <TouchableOpacity onPress={LoginUser} style={styles.button} disabled={loading}>
+                    <Text style={styles.buttonText}>Connexion</Text>
+                </TouchableOpacity>
+            )}
         </View>
     );
 }
--- ClimaSphere/pages/Signup/Signup.jsx
+++ ClimaSphere/pages/Signup/Signup.jsx
@@ -1,81 +1,54 @@
+// Signup.js
 import React, { useState } from 'react';
-import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 import { styles } from './SignupStyle';
 import Checkbox from 'expo-checkbox';
+import { auth } from '../../firebaseConfig';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Signup() {
-    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [agree, setAgree] = useState(false);
-    const [usernameError, setUsernameError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
-
-    const validateUsername = () => {
-        if (username.length === 0) {
-            setUsernameError("Le nom d'utilisateur ne peut pas être vide.");
-        } else {
-            setUsernameError('');
+    const RegisterUser = async () => {
+        if (!email || !password || !agree) {
+            setError('Please fill all fields and agree to the terms.');
+            return;
         }
-    };
 
-    const validateEmail = () => {
-
-        const emailRegex = /\S+@\S+\.\S+/;
-        if (!emailRegex.test(email)) {
-            setEmailError('Adresse email invalide.');
-        } else {
-            setEmailError('');
-        }
-    };
-
-    const validatePassword = () => {
-        if (password.length < 6) {
-            setPasswordError('Le mot de passe doit contenir au moins 6 caractères.');
-        } else {
-            setPasswordError('');
+        setLoading(true);
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            Alert.alert('User Created Successfully');
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false);
         }
     };
 
     return (
         <View style={styles.container}>
             <Image source={require('../../assets/Signup.png')} style={styles.backgroundImage} />
-            <Text style={styles.header}>Ravi de vous voir!</Text>
-            <Text style={styles.subHeader}>Créez votre compte</Text>
+            <Text style={styles.header}>Sign Up</Text>
 
             <TextInput
                 style={styles.input}
-                placeholder="Entrez votre nom d'utilisateur"
-                placeholderTextColor="#ffffff"
-                onChangeText={setUsername}
-                onEndEditing={validateUsername}
-                value={username}
-            />
-            {!!usernameError && <Text style={styles.error}>{usernameError}</Text>}
-
-            <TextInput
-                style={styles.input}
-                placeholder="Entrez votre adresse email"
+                placeholder="Email"
                 keyboardType="email-address"
-                placeholderTextColor="#ffffff"
                 onChangeText={setEmail}
-                onEndEditing={validateEmail}
                 value={email}
             />
-            {!!emailError && <Text style={styles.error}>{emailError}</Text>}
-
             <TextInput
                 style={styles.input}
-                placeholder="Tapez votre mot de passe"
+                placeholder="Password"
                 secureTextEntry
-                placeholderTextColor="#ffffff"
                 onChangeText={setPassword}
-                onEndEditing={validatePassword}
                 value={password}
             />
-            {!!passwordError && <Text style={styles.error}>{passwordError}</Text>}
 
             <View style={styles.checkboxContainer}>
                 <Checkbox
@@ -83,12 +56,17 @@ export default function Signup() {
                     onValueChange={setAgree}
                     style={styles.checkbox}
                 />
-                <Text style={styles.checkboxLabel}>J'accepte les termes et conditions</Text>
+                <Text style={styles.checkboxLabel}>I agree to the terms and conditions</Text>
             </View>
 
-            <TouchableOpacity style={styles.button} disabled={!agree}>
-                <Text style={styles.buttonText}>Crée mon compte</Text>
-            </TouchableOpacity>
+            {loading ? (
+                <ActivityIndicator size="large" />
+            ) : (
+                <TouchableOpacity onPress={RegisterUser} style={styles.button} disabled={!agree}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+            )}
+            {error ? <Text style={styles.error}>{error}</Text> : null}
         </View>
     );
 }
