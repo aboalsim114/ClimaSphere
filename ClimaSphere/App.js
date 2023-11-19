7240178007dff8d1777fad1c875d8505ab39cf2b
“aboalsim114” <aboalsim114@gmail.com>
2023-11-19 16:58:57 +0100

    fix navigation Routes

--- ClimaSphere/App.js
+++ ClimaSphere/App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigator from "./routes/HomeStack";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import WelcomePage from "./pages/WelcomePage/WelcomePage.jsx";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+
+
+  const RouteOptions = {
+
+    headerTitle: '',
+    headerTintColor: '#fff',
+    headerTransparent: true,
+  }
+
+
   return (
-    <Navigator />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='WelcomePage'>
+        <Stack.Screen name="WelcomePage" component={WelcomePage} options={RouteOptions} />
+        <Stack.Screen name="Login" component={Login} options={RouteOptions} />
+        <Stack.Screen name="Signup" component={Signup} options={RouteOptions} />
+      </Stack.Navigator>
+
+      <StatusBar style="auto" />
+
+
+
+    </NavigationContainer>
   );
 }
 
