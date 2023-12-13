0db1957a3e428d62e930157f6611ee7c7e0cf6ce
“aboalsim114” <aboalsim114@gmail.com>
2023-12-13 15:48:47 +0100

    mettre en place le Signout et correction de Connexion

--- ClimaSphere/App.js
+++ ClimaSphere/App.js
@@ -1,48 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native'; // Importez View pour un écran vide
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import WelcomePage from "./pages/WelcomePage/WelcomePage.jsx";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-
+import Profile from "./pages/Profile/Profile";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
+function AuthenticatedTabNavigator({ setIsAuthenticated }) {
+  const handleSignOut = async () => {
+    await AsyncStorage.removeItem('@auth_token');
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Profile') {
+            iconName = focused ? 'ios-person' : 'ios-person-outline';
+          } else if (route.name === 'SignOut') {
+            iconName = 'ios-exit';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="SignOut"
+        component={View} // Un écran vide
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+            handleSignOut();
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('@auth_token');
+      setIsAuthenticated(!!token);
+    };
 
-  const RouteOptions = {
-
-    headerTitle: '',
-    headerTintColor: '#fff',
-    headerTransparent: true,
-  }
-
+    checkToken();
+  }, []);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='WelcomePage'>
-        <Stack.Screen name="WelcomePage" component={WelcomePage} options={RouteOptions} />
-        <Stack.Screen name="Login" component={Login} options={RouteOptions} />
-        <Stack.Screen name="Signup" component={Signup} options={RouteOptions} />
+        {!isAuthenticated ? (
+          <>
+            <Stack.Screen name="WelcomePage" component={WelcomePage} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+          </>
+        ) : (
+          <Stack.Screen name="Authenticated">
+            {() => <AuthenticatedTabNavigator setIsAuthenticated={setIsAuthenticated} />}
+          </Stack.Screen>
+        )}
       </Stack.Navigator>
-
-      <StatusBar style="auto" />
-
-
-
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- ClimaSphere/pages/Login/Login.jsx
+++ ClimaSphere/pages/Login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator, Alert } from "react-native";
 import { styles } from "./LoginStyle";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -10,24 +12,34 @@ export default function Login() {
     const [emailError, setEmailError] = useState("");
     const [passwordError, setPasswordError] = useState("");
     const auth = getAuth();
+    const navigation = useNavigation();
 
     const LoginUser = async () => {
         if (!validateEmail() || !validatePassword()) return;
         setLoading(true);
 
         try {
-            await signInWithEmailAndPassword(auth, email, password);
-            Alert.alert("connexion reussi")
-        } catch (error) {
-            if (error.code === 'auth/user-not-found') {
-                setEmailError('No user found with this email.');
-            } else if (error.code === 'auth/wrong-password') {
-                setPasswordError('Incorrect password.');
-            } else {
-                setEmailError('Failed to login. Please try again.');
-            }
-        } finally {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log("User signed in", userCredential); // Pour le debugging
+            const token = await userCredential.user.getIdToken();
+            await AsyncStorage.setItem('@auth_token', token); // Stockage du token
+            navigation.navigate('Authenticated', { screen: 'Profile' }); // Correct navigation
             setLoading(false);
+        } catch (error) {
+            console.error("Login error", error); // Pour le debugging
+            setLoading(false);
+            handleLoginError(error);
+        }
+    };
+
+    const handleLoginError = (error) => {
+        console.error("Login error handler", error); // Pour le debugging
+        if (error.code === 'auth/user-not-found') {
+            setEmailError('Aucun utilisateur trouvé avec cet email.');
+        } else if (error.code === 'auth/wrong-password') {
+            setPasswordError('Mot de passe incorrect.');
+        } else {
+            Alert.alert('Erreur de connexion', error.message);
         }
     };
 
